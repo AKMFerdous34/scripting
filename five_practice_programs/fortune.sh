29c504a8c8e0cd3c83201f980ff1d015a39d3cbc
Ferdous <akmferdous97@gmail.com>
2025-02-01 23:41:59 +0600

    updated the fortune file with almost final touches

--- five_practice_programs/fortune.sh
+++ five_practice_programs/fortune.sh
@@ -4,6 +4,24 @@
 
 echo -e "\n ~~ Fortune Teller ~~ \n"
 
+QUESTION=""
 RESPONSES=("yes" "no" "Maybe" "Outlook good" "Dont count on it" "Ask again Later");
 
-echo ${RESPONSES[5]}  
+N=$((RANDOM%6))
+
+GET_FORTUNE(){
+if [[ ! $1 ]] then
+  echo -e " \n Ask a yes or no question: \n "
+else
+  echo "Try again. Make sure it ends with a question mark:"
+fi
+
+read QUESTION
+}
+
+until [[ $QUESTION =~ \?$ ]] do
+  GET_FORTUNE sadasd 
+  #again
+done 
+
+echo -e "\n ${RESPONSES[N]} \n"
